68e01acd1b07f2306b6808e1416e0dc87f3c6674
Ariel Pineiro <arielsrv@gmail.com>
2019-06-24 19:43:34 -0300

    Agrego ejemplo de dispatch.

---
+++ Clase5/PiedraPapelTijera/Program.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+
+namespace PiedraPapelTijera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejemplo de Double Dispatch
+
+            Hand hand1 = new Rock();
+            Hand hand2 = new Paper();
+
+            // Primer dispatch es LoseAgainst (dependendiendo de la instancia, es este caso una Rock())
+            Console.WriteLine(hand1.LoseAgainst(hand2));
+        }
+    }
+
+
+    abstract class Hand
+    {
+        public abstract bool LoseAgainst(Hand hand);
+        public abstract bool LoseAgainst(Rock rock);
+        public abstract bool LoseAgainst(Paper paper);
+        public abstract bool LoseAgainst(Scissors scissors);
+
+        // public abstract bool LoseAgainst(Wood wood);
+    }
+
+    class Rock : Hand
+    {
+        public override bool LoseAgainst(Rock rock)
+        {
+            return false;
+        }
+
+        public override bool LoseAgainst(Paper paper)
+        {
+            return true;
+        }
+
+        public override bool LoseAgainst(Scissors scissors)
+        {
+            return false;
+        }
+
+        public override bool LoseAgainst(Hand hand)
+        {
+            // Segundo dispatch es LoseAgainst (dependendiendo de la instancia, es este caso una Paper())
+            return !hand.LoseAgainst(this);
+        }
+    }
+
+    class Paper : Hand
+    {
+        public override bool LoseAgainst(Rock rock)
+        {
+            return false;
+        }
+
+        public override bool LoseAgainst(Paper paper)
+        {
+            return false;
+        }
+
+        public override bool LoseAgainst(Scissors scissors)
+        {
+            return true;
+        }
+
+        public override bool LoseAgainst(Hand hand)
+        {            
+            return !hand.LoseAgainst(this);
+        }
+    }
+
+    class Scissors : Hand
+    {
+        public override bool LoseAgainst(Rock rock)
+        {
+            return true;
+        }
+
+        public override bool LoseAgainst(Paper paper)
+        {
+            return false;
+        }
+
+        public override bool LoseAgainst(Scissors scissors)
+        {
+            return false;
+        }
+
+        public override bool LoseAgainst(Hand hand)
+        {
+            return !hand.LoseAgainst(this);
+        }
+    }
+}
